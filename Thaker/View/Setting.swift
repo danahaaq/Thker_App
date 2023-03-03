2140b5c82b42bfd6f705612cd257f59e053d6e66
Aldanah Alqabbani <danaalialq@gmail.com>
2023-03-03 16:56:23 +0300

    - Setting View is Done ! - Comment is added with the future work (Notification, share button)

--- Thaker/View/Setting.swift
+++ Thaker/View/Setting.swift
@@ -21,6 +21,7 @@ struct Setting: View {
     @State private var night = false
     
     @State var notificationsOn: Bool = false
+    
     var body: some View {
         NavigationView{
             VStack{
@@ -33,97 +34,101 @@ struct Setting: View {
                             Toggle("الاذكار", isOn: $noti_thker)
                                 .onChange(of: noti_thker) { value in
                                     auth()
-                                    
                                 }
                         }
                         if noti_thker{
                             Toggle("اذكار الصباح", isOn: $Morning)
                                 .onChange(of: Morning) { value in
                                     scheduleLocal()
-                                    
                                 }
                         }
-                        
-                        Section("حول التطبيق"){
-                            VStack(alignment: .leading,spacing: 3){
-                                HStack(alignment: .center, spacing: 10){
-                                    Image(systemName: "books.vertical")
-                                        .foregroundColor(Color(red: 0.6, green: 0.6, blue: 0.6))
-                                    Text("المرجع")
-                                }
-                                Text("كتاب حصن المسلم من أذكار الكتاب والسنه")
-                                    .font(.caption)
+                    }
+                    Section("حول التطبيق"){
+                        VStack(alignment: .leading,spacing: 3){
+                            HStack(alignment: .center, spacing: 10){
+                                Image(systemName: "books.vertical")
                                     .foregroundColor(Color(red: 0.6, green: 0.6, blue: 0.6))
+                                Text("المرجع")
                             }
-                            Button(action: {
-                                if MFMailComposeViewController.canSendMail() {
-                                    self.isShowingMailView.toggle()
-                                } else {
-                                    print("Can't send emails from this device")
-                                }
-                                if result != nil {
-                                    print("Result: \(String(describing: result))")
-                                }
-                            }) {
-                                HStack(alignment: .center, spacing: 10){
-                                    Image(systemName: "paperplane")
-                                        .foregroundColor(Color(red: 0.6, green: 0.6, blue: 0.6))
-                                    Text("تواصل معنا")
-                                        .foregroundColor(colorScheme == .dark ? .white : .black)
-                                }
+                            Text("كتاب حصن المسلم من أذكار الكتاب والسنه")
+                                .font(.caption)
+                                .foregroundColor(Color(red: 0.6, green: 0.6, blue: 0.6))
+                        }
+                        Button(action: {
+                            // Email
+                            if MFMailComposeViewController.canSendMail() {
+                                self.isShowingMailView.toggle()
+                            } else {
+                                print("Can't send emails from this device")
                             }
-                            .sheet(isPresented: $isShowingMailView) {
-                                MailView(result: $result) { composer in
-                                    composer.setSubject("تطبيق ذِكر")
-                                    composer.setToRecipients(["danaalialq@gmail.com"])
-                                }
+                            if result != nil {
+                                print("Result: \(String(describing: result))")
+                            }
+                        }) {
+                            HStack(alignment: .center, spacing: 10){
+                                Image(systemName: "paperplane")
+                                    .foregroundColor(Color(red: 0.6, green: 0.6, blue: 0.6))
+                                Text("تواصل معنا")
+                                    .foregroundColor(colorScheme == .dark ? .white : .black)
+                            }
+                        }
+                        .sheet(isPresented: $isShowingMailView) {
+                            MailView(result: $result) { composer in
+                                composer.setSubject("تطبيق ذِكر")
+                                composer.setToRecipients(["danaalialq@gmail.com"])
                             }
                         }
                     }
                 }
-                .navigationTitle("الاعدادات")
-                .navigationBarBackButtonHidden(true)
-                .toolbar{
-                    ToolbarItem(placement: .navigationBarTrailing){
-                        Button{
-                            pres = true
-                        }label:{
-                            Text("تم")
-                                .foregroundColor(Color.blue)
-                        }.fullScreenCover(isPresented:$pres){
-                            MainPage()
-                        }
+            }
+            .navigationTitle("الاعدادات")
+            .navigationBarBackButtonHidden(true)
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button{
+                        pres = true
+                    }label:{
+                        Text("تم")
+                            .foregroundColor(Color.blue)
+                    }.fullScreenCover(isPresented:$pres){
+                        MainPage()
                     }
                 }
-            }.environment(\.layoutDirection, .rightToLeft)
-        }
+            }
+        }.environment(\.layoutDirection, .rightToLeft)
     }
+}
+
+// Notification future work:
+// - Sava the state of the toggle
+// - Add multible notification per day
+// - Reperat a single notification
+
     // Notification aithorization
-    func auth(){
-        let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-            if success {
-                print("All set!")
-            } else if let error = error {
-                print(error.localizedDescription)
-            }
+func auth(){
+    let center = UNUserNotificationCenter.current()
+    center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+        if success {
+            print("All set!")
+        } else if let error = error {
+            print(error.localizedDescription)
         }
     }
-    // Schedule Notification 
-    func scheduleLocal() {
-        let notificationContent = UNMutableNotificationContent()
-        notificationContent.title = "اذكار الصباح"
-        notificationContent.body = "لا تنسى عزيزي اذكار الصباح فهي حفظ من شرور الدنيا والنفس ووسوسة الشيطان "
-        notificationContent.sound = .default
-        var datComp = DateComponents()
-        datComp.hour = 15
-        datComp.minute = 24
-        let trigger = UNCalendarNotificationTrigger(dateMatching: datComp, repeats: true)
-        let request = UNNotificationRequest(identifier: "ID", content: notificationContent, trigger: trigger)
-        UNUserNotificationCenter.current().add(request) { (error : Error?) in
-            if let theError = error {
-                print(theError.localizedDescription)
-            }
+}
+// Schedule Notification
+func scheduleLocal() {
+    let notificationContent = UNMutableNotificationContent()
+    notificationContent.title = "اذكار الصباح"
+    notificationContent.body = "لا تنسى عزيزي اذكار الصباح فهي حفظ من شرور الدنيا والنفس ووسوسة الشيطان "
+    notificationContent.sound = .default
+    var datComp = DateComponents()
+    datComp.hour = 16
+    datComp.minute = 50
+    let trigger = UNCalendarNotificationTrigger(dateMatching: datComp, repeats: true)
+    let request = UNNotificationRequest(identifier: "ID", content: notificationContent, trigger: trigger)
+    UNUserNotificationCenter.current().add(request) { (error : Error?) in
+        if let theError = error {
+            print(theError.localizedDescription)
         }
     }
 }
@@ -133,7 +138,8 @@ struct Setting_Previews: PreviewProvider {
     }
 }
 
-//https://stackoverflow.com/questions/56784722/swiftui-send-email
+// Email feature
+// https://stackoverflow.com/questions/56784722/swiftui-send-email
 public struct MailView: UIViewControllerRepresentable {
     @Environment(\.presentationMode) var presentation
     @Binding var result: Result<MFMailComposeResult, Error>?
